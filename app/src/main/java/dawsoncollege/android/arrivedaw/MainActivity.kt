7c49e196a55470a612575a4fce389ada367c80b8
Jean-Claude Desrosiers <jdesrosiers@dawsoncollege.qc.ca>
2022-09-05 23:31:40 -0400

    Add starting code for the project

---
+++ app/src/main/java/dawsoncollege/android/arrivedaw/MainActivity.kt
@@ -0,0 +1,52 @@
+package dawsoncollege.android.arrivedaw
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import com.google.zxing.qrcode.QRCodeWriter
+import dawsoncollege.android.arrivedaw.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val qrTestString = "Hello this is a test string! It will be used to generate a QR code"
+        binding.QRResult.setImageBitmap(encodeStringToBitmap(qrTestString))
+    }
+
+    @Throws(WriterException::class)
+    fun encodeStringToBitmap(str: String): Bitmap {
+        /* First encode the string into a bit matrix... */
+        val bitMatrix: BitMatrix = QRCodeWriter().encode(str, BarcodeFormat.QR_CODE, 400, 400)
+
+        val pixels = IntArray(bitMatrix.width * bitMatrix.height)
+
+        /* ... then translate the bit matrix into a grid of pixels ... */
+        for (y in 0 until bitMatrix.height) {
+            val offset = y * bitMatrix.width
+
+            for (x in 0 until bitMatrix.width) {
+                if (bitMatrix[x, y]) {
+                    pixels[offset + x] = Color.BLACK
+                } else {
+                    pixels[offset + x] = Color.WHITE
+                }
+            }
+        }
+
+        /* ... finally put the grid of pixels into a bitmap image */
+        val bitmap: Bitmap =
+            Bitmap.createBitmap(bitMatrix.width, bitMatrix.height, Bitmap.Config.ARGB_8888)
+        bitmap.setPixels(pixels, 0, bitMatrix.width, 0, 0, bitMatrix.width, bitMatrix.height)
+
+        return bitmap
+    }
+}
